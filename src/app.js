6238187479cfeec931a25ecffea8be92c55c442d
Natansh14687 <natansh522@gmail.com>
2025-09-30 15:44:38 +0530

    exploring routes

--- src/app.js
+++ src/app.js
@@ -2,17 +2,35 @@ const express = require("express");
 
 const app = express();
 
-app.use("/hii",(req,res)=>{
-    res.end("Hello, World!!");
+// app.use("/user", (req, res)=>{
+//     res.send("HAHAHAHAHAHAHHAHAHAHA");
+// });
+
+app.get("/user", (req, res) => {
+    res.send({firstName: "Natansh", lastName:"Khurana"});
+})
+
+app.post("/user",(req, res) => {
+    res.send("Posting data to database");
+})
+
+app.delete("/user", (req, res) => {
+    res.send("Deleting data...");
+})
+
+app.patch("/user", (req, res)=>{
+    res.send("updating fields in document in database");
+})
+
+app.put("/user", (req, res)=>{
+    res.send("Updating database");
 })
 
 app.use("/test", (req, res)=>{
     res.send("This is the testing route");
 })
 
-app.use("/",(req, res)=>{
-    res.end("This is the dashboard page");
-})
+
 
 app.listen(7777, ()=>{
     console.log("Server is successfully listening to port 7777");
