d84968179169cb149cc17ab0c3a788310ad75965
Natansh14687 <natansh522@gmail.com>
2025-10-07 16:41:56 +0530

    right way of connecting databse and listening server

--- src/app.js
+++ src/app.js
@@ -2,11 +2,18 @@ const express = require("express");
 
 const app = express();
 
-const {connectDB} = require("./config/database")
+const { connectDB } = require("./config/database");
 
-connectDB();
+// The right way of connecting to database means database will be connected first and then server will be started if database is not connected it will give error
 
 
-app.listen(7777, () => {
-  console.log("Server is successfully listening to port 7777");
-});
+connectDB()
+  .then(() => {
+    console.log("Database connection established");
+    app.listen(7777, () => {
+      console.log("Server is successfully listening to port 7777");
+    });
+  })
+  .catch((err) => {
+    console.log("Database can't be connected");
+  });
