bea5b52fb66891aee211429c3692ae412378c5d3
Natansh14687 <natansh522@gmail.com>
2025-10-07 14:09:58 +0530

    dynamic and query routing but string pattern deprecated

--- src/app.js
+++ src/app.js
@@ -6,33 +6,17 @@ const app = express();
 //     res.send("HAHAHAHAHAHAHHAHAHAHA");
 // });
 
-app.get("/user", (req, res) => {
-    res.send({firstName: "Natansh", lastName:"Khurana"});
-})
+app.get("/abc", (req, res) => {
+  console.log(req.query);
+  res.send({ firstName: "Natansh", lastName: "Khurana" });
+});
 
-app.post("/user",(req, res) => {
-    res.send("Posting data to database");
-})
-
-app.delete("/user", (req, res) => {
-    res.send("Deleting data...");
-})
-
-app.patch("/user", (req, res)=>{
-    res.send("updating fields in document in database");
-})
-
-app.put("/user", (req, res)=>{
-    res.send("Updating database");
-})
-
-app.use("/test", (req, res)=>{
-    res.send("This is the testing route");
-})
-
-
-
-app.listen(7777, ()=>{
-    console.log("Server is successfully listening to port 7777");
+app.get("/user/:userId/:name/:password", (req, res)=>{
+    console.log(req.params);
     
+    res.send("getting dynamic routes");
+})
+
+app.listen(7777, () => {
+  console.log("Server is successfully listening to port 7777");
 });
