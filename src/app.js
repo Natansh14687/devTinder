b10a32a816ff82c1d3535730e805c6023aac9c97
Natansh14687 <natansh522@gmail.com>
2025-10-07 15:26:23 +0530

    authorization for every admin route is clumsy

--- src/app.js
+++ src/app.js
@@ -4,29 +4,41 @@ const app = express();
 
 
 
-// app.get("/user",(req, res, next)=>{
-//     console.log("handling route user 1 !!");
-//     res.send("response 1");
-//     next();
-// }, (req, res) => {
-//     console.log("Handling route user 2 !!");
-//     res.send("response 2"); 
-// }, (req, res) => {
-//     console.log("Handling route user 3 !!");
-//     res.send("response 3");
-// })
+app.use("/user",(req, res, next)=>{
+    console.log("Reponse from user 1");
+    res.send("user 1 !!");
+    next();
+})
 
-app.get("/user",(req, res, next)=>{
-    console.log("handling route user 1 !!");
-    res.send("response 1");
-    next();
-}, (req, res, next) => {
-    console.log("Handling route user 2 !!");
-    // res.send("response 2");
-    next();
-}, (req, res) => {
-    console.log("Handling route user 3 !!");
-    // res.send("response 3");
+app.use("/user", (req, res)=>{
+    console.log("Response from user 2");
+    // res.send("user 2 !!");
+    
+})
+
+// Authorization for every route in admin root route is very clumsy, see
+
+app.get("/admin/getAllData", (req, res)=>{
+    console.log("Get all data");
+    const token = "xyz";
+    const isAdminAuthorized = token === "xy";
+    if(isAdminAuthorized){
+        res.send("get All Data");
+    }else{
+        res.status(401).send("Unauthorized Request");
+    }
+    
+});
+
+app.get("/admin/deleteAllData", (req, res)=>{
+    console.log("Delete all data");
+    const token = "xyz";
+    const isAdminAuthorized = token === "xyz";
+    if(isAdminAuthorized){
+        res.send("Delete all data");
+    }else{
+        res.status(401).send("Unauthorized Request");
+    }
 })
 
 app.listen(7777, () => {
