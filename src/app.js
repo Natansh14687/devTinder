377faf61f8a576a558ae5851230f6294ddd9304b
Natansh14687 <natansh522@gmail.com>
2025-10-12 16:04:02 +0530

    query data in database using mongoose queries apis

--- src/app.js
+++ src/app.js
@@ -4,25 +4,60 @@ const app = express();
 
 const { connectDB } = require("./config/database");
 const User = require("./models/users");
+app.use(express.json());
 
-app.post("/signup", async (req, res)=>{
+// app.post("/signup", async (req, res)=>{
 
-    const user = new User({
-        firstName: "Natansh",
-        lastName: "Khurana",
-        emailId: "natansh@gmail.com",
-        password: "hello@123",
-        age: 22,
-        gender: "male",
-    })
+//     const user = new User(req.body);
 
-    try{
-        await user.save();
-        res.send("User added successfully");
-    }catch(err){
-        res.status(400).send("error ocurred in connecting database" + err.message);
+//     try{
+//         await user.save();
+//         res.send("User added successfully");
+//     }catch(err){
+//         res.status(400).send("error ocurred in connecting database" + err.message);
+//     }
+// })
+
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+
+  try {
+    const users = await User.find({ emailId: userEmail });
+    if (users.length == 0) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(users);
     }
-})
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+
+  try {
+    const user = await User.findByIdAndDelete({ _id: userId });
+    res.send("User deleted successfully");
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+// Update data of the user
+
+app.patch("/user", async (req, res) => {
+  const userId = req.body?.userId;
+  const data = req.body;
+
+  try {
+    const user = await User.findByIdAndUpdate(userId, data, {returnDocument:"after"});
+    console.log(user);
+    res.send("user updated successfully");
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
 
 // The right way of connecting to database means database will be connected first and then server will be started if database is not connected it will give error
 connectDB()
